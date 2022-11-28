ea27adcc77cf9eb282109be98ff6f42f8c7774bf
jcsotom <jcsotom@localhost>
2022-11-27 21:46:14 -0700

    new changes

---
+++ AppPruebaCodigo/src/apppruebacodigo/User.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package apppruebacodigo;
+
+/**
+ *
+ * @author jcsotom
+ */
+public class User {
+    String name;
+    String password;
+    
+    public User(String _name, String pass){
+        this.name = _name;
+        this.password = pass;
+    }
+}
